e72d0b67dea61f8f386bca298854a6a5535b593d
alexbocharov98 <a.bocharov34@yandex.ru>
2024-10-05 09:52:38 +0300

    Homework8 Исправление1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("task1,2");
 
-        int[] integer = new int[]{1, 2, 3};
+        int[] integer = new int[3];
         integer[0] = 1;
         integer[1] = 2;
         integer[2] = 3;
@@ -16,12 +16,12 @@ public class Main {
             System.out.print(integer[i] + ", ");
         }
         double[] fractionalNumber = {1.57, 7.654, 9.986};
-        for (int i1 = 0; i1 < fractionalNumber.length; i1++) {
-            if (i1 == fractionalNumber.length - 1) {
-                System.out.println(fractionalNumber[i1]);
+        for (int i = 0; i < fractionalNumber.length; i++) {
+            if (i == fractionalNumber.length - 1) {
+                System.out.println(fractionalNumber[i]);
                 break;
             }
-            System.out.print(fractionalNumber[i1] + ", ");
+            System.out.print(fractionalNumber[i] + ", ");
         }
         byte[] myMassiv = {1, 2, 3, 4, 5};
         for (int i2 = 0; i2 < myMassiv.length; i2++) {
@@ -35,11 +35,12 @@ public class Main {
 
         for (int i4 = integer.length - 1; i4 >= 0; i4--) {
             if (i4 < 0) {
-                System.out.println(integer[i4]);
+                System.out.print(integer [i4]);
                 break;
             }
             System.out.print(integer[i4] + ", ");
         }
+        System.out.println();
 
         for (int i1 = fractionalNumber.length - 1; i1 >= 0; i1--) {
             if (i1 < 0) {
@@ -48,6 +49,8 @@ public class Main {
             }
             System.out.print(fractionalNumber[i1] + ", ");
         }
+        System.out.println();
+    
 
         for (int i2 = myMassiv.length - 1; i2 >= 0; i2--) {
             if (i2 < 0) {
@@ -56,17 +59,17 @@ public class Main {
             }
             System.out.print(myMassiv[i2] + ", ");
         }
+        System.out.println();
+
 
         System.out.println("task4");
 
         for (int i = 0; i < 3; i++) {
             if (integer[i] % 2 != 0)
-                integer[i] = integer[i] + 1;
+                integer[i] = integer[i]++;
             {
             }
         }
         System.out.println(Arrays.toString(integer));
-
-
     }
 }
